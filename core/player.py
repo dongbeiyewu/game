57643e61b69160e93b69e1381307d0bee74db35c
ming <2698922550@qq.com>
2024-08-25 23:25:57 +0800

    add collision detection

--- core/player.py
+++ core/player.py
@@ -1,13 +1,13 @@
-from ursina import Entity, Vec3, held_keys, time, camera, color
+from ursina import Entity, Vec3, held_keys, time, camera, color, boxcast
 
 class Player(Entity):
     def __init__(self):
         super().__init__(
-            model='cube',
-            scale=(1, 2, 1),
+            model='cube',  # 替换为你的小人模型
+            scale=(0.5, 1, 0.5),
             color=color.orange,
             position=(0, 1, 0),
-            collider='box'
+            collider='box'  # 添加碰撞检测
         )
 
     def update(self):
@@ -17,9 +17,16 @@ class Player(Entity):
             0,
             held_keys['w'] - held_keys['s']   # 前后方向
         ).normalized()
-        self.position += direction * speed
         
-        # 设置摄像机位置为角色上方和后方，创建2.5D视角
+        # 计算移动后的新位置
+        new_position = self.position + direction * speed
+        
+        # 检查是否会发生碰撞
+        hit_info = boxcast(self.position, direction, distance=speed, ignore=[self], thickness=(0.5, 1, 0.5))
+        
+        if not hit_info.hit:
+            self.position = new_position  # 如果没有碰撞，更新位置
+        
+        # 更新摄像机位置
         camera.position = self.position + Vec3(0, 20, -20)
-        camera.rotation_x = 30  # 俯视角度
-        camera.look_at(self.position)  # 摄像机看向角色
+        camera.look_at(self.position)
